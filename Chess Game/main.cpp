421ea652bd7b97f2d8e58f77c02bfd9944751c23
Buhsra <f250597@cfd.nu.edu.pk>
2026-05-06 23:15:53 +0500

    Another member has done error handling

--- Chess Game/main.cpp
+++ Chess Game/main.cpp
@@ -1,5 +1,4 @@
 #include "piece.h"//including header file
-
 // ================= GLOBAL VARIABLES (for special moves) =================
 int lastDoublePawnRow = -1;
 int lastDoublePawnCol = -1;
@@ -12,197 +11,178 @@ bool whiteRookAMoved = false; // queenside rook
 bool whiteRookHMoved = false; // kingside rook
 bool blackRookAMoved = false;
 bool blackRookHMoved = false;
-
 int main()
 {
-    // we will be creating an 8x8 matrix that is initially empty
     Piece* board[8][8] = { nullptr };
 
-    //After creating the space the chess pieces place & positions will be added through array of pointers 
-    // creating dynamic memory and intializing the pawns 
-    // Giving the position info and appearence of the piece 
     for (int i = 0; i < 8; i++) {
         board[1][i] = new Pawn("black", 1, i, 'P');
         board[6][i] = new Pawn("white", 6, i, 'P');
     }
 
-    // creating dynamic memory and intializing the Rooks
-    // Giving the position info and appearence of the piece 
     board[0][0] = new Rook("black", 0, 0, 'R');
     board[0][7] = new Rook("black", 0, 7, 'R');
     board[7][0] = new Rook("white", 7, 0, 'R');
     board[7][7] = new Rook("white", 7, 7, 'R');
 
-    // creating dynamic memory and intializing the Knights
-    // Giving the position info and appearence of the piece 
     board[0][1] = new Knight("black", 0, 1, 'N');
     board[0][6] = new Knight("black", 0, 6, 'N');
     board[7][1] = new Knight("white", 7, 1, 'N');
     board[7][6] = new Knight("white", 7, 6, 'N');
 
-    // creating dynamic memory and intializing the Bishop
-    // Giving the position info and appearence of the piece 
     board[0][2] = new Bishop("black", 0, 2, 'B');
     board[0][5] = new Bishop("black", 0, 5, 'B');
     board[7][2] = new Bishop("white", 7, 2, 'B');
     board[7][5] = new Bishop("white", 7, 5, 'B');
 
-    // creating dynamic memory and intializing the Queens
-    // Giving the position info and appearence of the piece 
     board[0][3] = new Queen("black", 0, 3, 'Q');
     board[7][3] = new Queen("white", 7, 3, 'Q');
 
-    // creating dynamic memory and intializing the Kings
-    // Giving the position info and appearence of the piece 
     board[0][4] = new King("black", 0, 4, 'K');
     board[7][4] = new King("white", 7, 4, 'K');
 
-    // now we are arranging pattern to show the chess board on console 
-    //info already saved in memory and in Matrix of 8x8
-    cout << "\n   A B C D E F G H" << endl;
-    cout << "  -----------------" << endl;
+    string turn = "white";
 
-    for (int i = 0; i < 8; i++) {
-        cout << 8 - i << " |";
-
-        for (int j = 0; j < 8; j++) {
-            if (board[i][j] != nullptr)
-                cout << board[i][j]->getSymbol() << " ";
-            else
-                cout << ". ";
-        }
-
-        cout << "| " << 8 - i << endl;
-    }
-
-    cout << "  -----------------" << endl;
-    cout << "   A B C D E F G H" << endl;
-
-    string turn = "white";//Turn variable
     while (true)
     {
-        // STEP 1: Variables for move
-        int fromRow, fromCol;
-        int toRow, toCol;
+        system("cls");
 
-        string from, to; // source position (where piece is)
-        // destination position (where to move)
+        cout << "\nCurrent Turn: " << turn << endl;
+
+        cout << "\n   A B C D E F G H" << endl;
+        cout << "  -----------------" << endl;
+
+        for (int i = 0; i < 8; i++) {
+            cout << 8 - i << " |";
+            for (int j = 0; j < 8; j++) {
+                if (board[i][j] != nullptr)
+                    cout << board[i][j]->getSymbol() << " ";
+                else
+                    cout << ". ";
+            }
+            cout << "| " << 8 - i << endl;
+        }
+
+        cout << "  -----------------" << endl;
+        cout << "   A B C D E F G H\n" << endl;
+
+        int fromRow, fromCol, toRow, toCol;
+        string from, to;
+        cout << "Example input: E2 E4" << endl;
         cout << "Enter FROM position (e.g., C2): ";
         cin >> from;
 
         cout << "Enter TO position (e.g., C3): ";
         cin >> to;
-
-
+        //******************************
+        // PHASE 3 start HERE
+        //==input Validation====
+        if (from.empty() || to.empty())
+        {
+            cout << "Empty input is not allowed!" << endl;
+            system("pause");
+            continue;
+        }
+        if (from.length() != 2 || to.length() != 2)
+            //for example it is like E23? then 
+        {
+            cout << "Invalid input format! Use format like E2" << endl;
+            system("pause");
+            continue;
+        }
+        //NOW CHECK COLOUMNS
+        if (toupper(from[0]) < 'A' || toupper(from[0]) > 'H' ||
+            toupper(to[0]) < 'A' || toupper(to[0]) > 'H')
+        {
+            cout << "Invalid column! Use A to H" << endl;
+            system("pause");
+            continue;
+        }
+        //Now Row CHECK
+        if (from[1] < '1' || from[1]>'8' || to[1] < '1' || to[1]>'8')
+        {
+            cout << "invalid Row ! use 1 to 8 " << endl;
+            system("pause");
+            continue;
+        }
 
         parseInput(from, fromRow, fromCol);
         parseInput(to, toRow, toCol);
-        // Validate input format
-
+        //********************* SAME POSTON CASE
+        // // ===== SAME POSITION CHECK =====
+        if (fromRow == toRow && fromCol == toCol)
+        {
+            cout << "Invalid move: Source and destination are same!" << endl;
+            system("pause");
+            continue;
+        }
+        // ===== INPUT VALIDATION ERROR =====
         if (fromRow < 0 || fromRow >= 8 || fromCol < 0 || fromCol >= 8 ||
             toRow < 0 || toRow >= 8 || toCol < 0 || toCol >= 8)
         {
             cout << "Invalid input!" << endl;
+            system("pause");
             continue;
         }
-        //Check source validity
-        // If there is no piece at selected position
+
+        // ===== NO PIECE ERROR =====
         if (board[fromRow][fromCol] == nullptr)
         {
             cout << "Invalid move: No piece at this position!" << endl;
-
-            // skip this iteration and ask again
+            system("pause");
             continue;
         }
 
-
-        // Next steps will go here
-        // - Check turn (white/black)
-
-        if ((board[fromRow][fromCol]->getColor()) == turn)
+        // ===== TURN ERROR =====
+        if (board[fromRow][fromCol]->getColor() != turn)
         {
-            cout << "Ok " << endl;
-
+            cout << "Not Your Turn" << endl;
+            system("pause");   // ADDED
+            continue;
         }
-        else
+
+        // ===== MOVE VALIDATION =====
+        if (board[fromRow][fromCol]->isValid_Move(toRow, toCol, board))
         {
-            cout << "Not Your Turn " << endl;
-            //RULE : Only white pieces move when turn is white, same for black”
-            continue;//skip this iteration , go back to input
-        }
-        // - Check valid move using isValid_Move()
-        if (board[fromRow][fromCol]->isValid_Move(toRow, toCol, board))            // Move Validation
-            //passing destination Row and Columns
-        {
-            // - Check destination cell
             if (board[toRow][toCol] == nullptr)
             {
-                // EMPTY CELL = normal move
-                /*  Move piece pointer
-                    Take piece from source
-                    Put it in destination*/
                 board[toRow][toCol] = board[fromRow][fromCol];
-
-                /*   Clear old square
-                     Make source square empty*/
                 board[fromRow][fromCol] = nullptr;
-                //Update piece position
-               // Tell piece its new row / col
                 board[toRow][toCol]->setPosition(toRow, toCol);
-                // Switch turn
-                if (turn == "white")
-                    turn = "black";
-                else
-                    turn = "white";
 
-                continue;//go back for next move
+                turn = (turn == "white") ? "black" : "white";
+                continue;
             }
             else
             {
-                //Same-color rejection
                 if (board[toRow][toCol]->getColor() == turn)
                 {
-                    cout << "Move is Rejected because it has same team Piece " << endl;
-                    continue;//go back to input
+                    cout << "Move rejected: same team piece" << endl;
+                    system("pause");
+                    continue;
                 }
                 else
                 {
-                    // CAPTURE CASE (enemy piece)
-
-                                    /* Replace enemy piece with current piece */
                     board[toRow][toCol] = board[fromRow][fromCol];
-
-                    /* Clear old square */
                     board[fromRow][fromCol] = nullptr;
-
-                    // Update position
                     board[toRow][toCol]->setPosition(toRow, toCol);
 
-                    // Switch turn
-                    if (turn == "white")
-                        turn = "black";
-                    else
-                        turn = "white";
-
-                    continue;//go back for next move
+                    turn = (turn == "white") ? "black" : "white";
+                    continue;
                 }
             }
-
-
         }
         else
         {
-            cout << "Invalid Move according to piece rules!" << endl;
+            cout << "Invalid move according to piece rules!" << endl;
+            system("pause");
         }
-
-        // - Move piece
     }
-    // Freeing memory of the 8x8 Matrix
+
     for (int i = 0; i < 8; i++)
     {
         for (int j = 0; j < 8; j++)
         {
-            //Best Practice to check that if it is null or occupied
             if (board[i][j] != nullptr)
             {
                 delete board[i][j];
